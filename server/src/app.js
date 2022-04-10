e32226786134925d2cc7258443ce20a3bba6087a
Gustavo <gustavo.motta23@gmail.com>
2022-04-10 14:10:17 -0300

    port changed

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,6 @@ app.use(cors())
 app.use('/', IndexRouter)
 app.use('/chat', ChatRouter)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
